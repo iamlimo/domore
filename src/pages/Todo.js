282853938779c4f29a8373734fa847c581f1b6fc
iamlimo <isaac.orija@gmail.com>
2023-01-02 16:08:39 +0100

    new branch

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Todo() {
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
-    <div>Todo</div>
-  )
+    
+    <div>
+        <input></input>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add a todo:
+          <input type="text" value={input} onChange={event => setInput(event.target.value)} />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Todo
+export default TodoList;
